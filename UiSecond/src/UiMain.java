7d0adca0ebd74e1c59fc76215fbbf210f3dbd877
Sivasurya <sivasurya@HP.(none)>
2014-12-05 18:02:55 +0100

    new files

--- UiSecond/src/UiMain.java
+++ UiSecond/src/UiMain.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.event.*;
 import java.awt.*;
@@ -7,6 +8,8 @@ import javax.swing.border.*;
 
 import java.awt.SystemColor;
 import java.io.File;
+
+import javax.swing.GroupLayout.Alignment;
 
 public class UiMain {
 	private JFrame frame;
@@ -22,9 +25,14 @@ public class UiMain {
 	private JButton btnDictionaryModel;
 	private JButton btnLanguageModel;
 	private JButton btnAcousticModel;
+	private String currentAsrSelected = null;
+	private boolean modelsNeeded = false;
+	private boolean dictLoadedLedCmu = false;
+	private boolean acousLoadedLedCmu = false;
+	private boolean langLoadedLedCmu = false;
 
 	public UiMain() {
-		initialize();
+		this.initialize();
 	}
 
 	private void initialize() {
@@ -32,22 +40,27 @@ public class UiMain {
 		frame.setBounds(100, 100, 662, 436);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		// Set UI to look more cool
+		try { 
+		    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
 
+		// objects for storing the properties
+		final UiAsrProperties cmuProperties = new UiAsrProperties();
+		final UiAsrProperties iSpeechProperties = new UiAsrProperties();
+		
 		// Combo box to select properties
 		comboAsrSelect = new JComboBox();
-		comboAsrSelect.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent actionAsrSelected) {
-				System.out.println(actionAsrSelected.getActionCommand());
-			}
-		});
-		comboAsrSelect.setModel(new DefaultComboBoxModel(new String[] {
-				"--- Select ---", "CmuSphinx", "iSpeech" }));
+		comboAsrSelect.setModel(new DefaultComboBoxModel(new String[] {"--- Select ---", "CmuSphinx", "iSpeech"}));
 		comboAsrSelect.setToolTipText("");
-		comboAsrSelect.setBounds(425, 98, 127, 61);
+		comboAsrSelect.setBounds(425, 120, 140, 39);
 		frame.getContentPane().add(comboAsrSelect);
-
+		
 		// Speech corpus path button
-		JButton btnSpeechCorpus = new JButton("Speech corpus");
+		final JButton btnSpeechCorpus = new JButton("Speech corpus");
 		btnSpeechCorpus.setBounds(220, 164, 141, 61);
 		frame.getContentPane().add(btnSpeechCorpus);
 
@@ -68,6 +81,8 @@ public class UiMain {
 
 		// Evaluation button
 		JButton btnEvaluate = new JButton("Evaluate");
+//		btnEvaluate.setEnabled(false);
+		btnEvaluate.setBackground(Color.BLUE);
 		btnEvaluate.setBounds(215, 282, 161, 61);
 		frame.getContentPane().add(btnEvaluate);
 
@@ -106,25 +121,81 @@ public class UiMain {
 		btnInstructions.setBounds(230, 115, 120, 27);
 		frame.getContentPane().add(btnInstructions);
 		
-		JButton button = new JButton("Select models");
-		button.setBounds(430, 180, 116, 27);
-		frame.getContentPane().add(button);
+		// Select models
+		final JLabel btnShow = new JLabel();
+		btnShow.setFont(new Font("DejaVu Sans", Font.PLAIN, 10));
+		btnShow.setBounds(400, 164, 194, 31);
+		frame.getContentPane().add(btnShow);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null), "Properties", TitledBorder.CENTER, TitledBorder.TOP, null, null));
-		panel.setBounds(390, 60, 210, 326);
-		frame.getContentPane().add(panel);
+		// led for loaded dictionary indication
+		final JLabel ledDict = new JLabel("•");
+		ledDict.setFont(new Font("DejaVu Sans", Font.PLAIN, 60));
+		ledDict.setForeground(Color.GREEN);
+		ledDict.setBounds(565, 230, 35, 28);
+		frame.getContentPane().add(ledDict);
+		ledDict.setVisible(false);
 
+		// led for loaded language model indication
+		final JLabel ledLang = new JLabel("•");
+		ledLang.setForeground(Color.GREEN);
+		ledLang.setFont(new Font("DejaVu Sans", Font.PLAIN, 60));
+		ledLang.setBounds(565, 280, 35, 28);
+		frame.getContentPane().add(ledLang);
+		ledLang.setVisible(false);
+		
+		// led for loaded acoustic model indication
+		final JLabel ledAcoustic = new JLabel("•");
+		ledAcoustic.setForeground(Color.GREEN);
+		ledAcoustic.setFont(new Font("DejaVu Sans", Font.PLAIN, 60));
+		ledAcoustic.setBounds(565, 330, 35, 28);
+		frame.getContentPane().add(ledAcoustic);
+		ledAcoustic.setVisible(false);
+		
+		// Combo box selection action
+		comboAsrSelect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent actionAsrSelected) {
+				JComboBox comboAsrSelected = (JComboBox)actionAsrSelected.getSource();
+				Object asrSelectedObj = comboAsrSelected.getSelectedItem();
+				if ("CmuSphinx".equals(asrSelectedObj)){
+					System.out.println("CmuSphnix is selected...");
+					modelsNeeded = true;
+					setSelectedAsr("cmusphinx");
+					btnShow.setText("Set the properties of " + currentAsrSelected);
+					btnShow.setVisible(true);
+					if (dictLoadedLedCmu)
+					ledDict.setVisible(true);
+					if (acousLoadedLedCmu)
+					ledAcoustic.setVisible(true);
+					if (langLoadedLedCmu)
+					ledLang.setVisible(true);
+				}
+				if ("iSpeech".equals(asrSelectedObj)){
+					System.out.println("iSpeech is selected...");
+					setSelectedAsr("ispeech");
+					modelsNeeded = false;
+					btnShow.setText("No models are needed for iSpeech ");
+					btnShow.setVisible(true);
+					ledDict.setVisible(false);
+					ledAcoustic.setVisible(false);
+					ledLang.setVisible(false);
+				}
+				if ("--- Select ---".equals(asrSelectedObj)){
+					System.out.println("Select is selected...");
+					setSelectedAsr(null);
+					btnShow.setText("Select anyone...");
+				}
+			}
+		});
+		
 		// Speech corpus path selection
 		btnSpeechCorpus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				File speechCorpusPathResult;
+				File speechCorpusPathResult = null;
 				speechCorpusChooser = new JFileChooser();
 				speechCorpusChooser.setCurrentDirectory(new java.io.File("."));
 				speechCorpusChooser.setDialogTitle(speechCorpusChoosertitle);
 				speechCorpusChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 				speechCorpusChooser.setAcceptAllFileFilterUsed(false);
-				
 				if (speechCorpusChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
 					speechCorpusPathResult = speechCorpusChooser.getSelectedFile();
 				}
@@ -134,7 +205,8 @@ public class UiMain {
 		// Dictionary path selection
 		btnDictionaryModel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				File dictionaryPathResult;
+				File dictionaryPathResult = null;
+				if (currentAsrSelected != null && modelsNeeded){
 				dictionaryChooser = new JFileChooser();
 				dictionaryChooser.setCurrentDirectory(new java.io.File("."));
 				dictionaryChooser.setDialogTitle(dictionaryChoosertitle);
@@ -143,14 +215,25 @@ public class UiMain {
 				
 				if (dictionaryChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
 					dictionaryPathResult = dictionaryChooser.getSelectedFile();
+					btnDictionaryModel.setBackground(Color.GREEN);
+					dictLoadedLedCmu = true;
+					ledDict.setVisible(true);
 				}
+				if(currentAsrSelected == "cmusphinx"){
+					cmuProperties.setUiDictionary(dictionaryPathResult);
+				}
+/*				if(currentAsrSelected == "ispeech" ){
+					iSpeechProperties.setUiDictionary(dictionaryPathResult);
+				}*/
 			}
+		}
 		});
 	
 		// Acoustic path selection
 		btnAcousticModel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				File acousticPathResult;
+				File acousticPathResult = null;
+				if(currentAsrSelected != null && modelsNeeded){
 				acousticChooser = new JFileChooser();
 				acousticChooser.setCurrentDirectory(new java.io.File("."));
 				acousticChooser.setDialogTitle(acousticChoosertitle);
@@ -159,14 +242,24 @@ public class UiMain {
 				
 				if (acousticChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
 					acousticPathResult = acousticChooser.getSelectedFile();
+					acousLoadedLedCmu = true;
+					ledAcoustic.setVisible(true);
 				}
+				if(currentAsrSelected == "cmusphinx"){
+					cmuProperties.setUiAcoustic(acousticPathResult);
+				}
+/*				if(currentAsrSelected == "ispeech"){
+					iSpeechProperties.setUiAcoustic(acousticPathResult);
+				}*/
 			}
+		}
 		});
 	
 		// Language path selection
 		btnLanguageModel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				File languagePathResult;
+				File languagePathResult = null;
+				if(currentAsrSelected != null && modelsNeeded){
 				languageChooser = new JFileChooser();
 				languageChooser.setCurrentDirectory(new java.io.File("."));
 				languageChooser.setDialogTitle(languageChoosertitle);
@@ -175,9 +268,24 @@ public class UiMain {
 				
 				if (languageChooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
 					languagePathResult = languageChooser.getSelectedFile();
+					langLoadedLedCmu = true;
+					ledLang.setVisible(true);
 				}
+				if(currentAsrSelected == "cmusphinx"){
+					cmuProperties.setUiLanguage(languagePathResult);
+				}
+/*				if(currentAsrSelected == "ispeech"){
+					iSpeechProperties.setUiLanguage(languagePathResult);
+				}*/
+			}
 			}
 		});
+	}
+	
+	
+	// function returning selected asr system
+	public void setSelectedAsr (String received){
+		currentAsrSelected = received;		
 	}
 	
 	// Main function
@@ -185,8 +293,9 @@ public class UiMain {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					UiMain window = new UiMain();
-					window.frame.setVisible(true);
+					UiMain cmuWindow = new UiMain();
+					cmuWindow.frame.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
